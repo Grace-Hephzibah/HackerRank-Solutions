a7fb1df39c761f636548f4d86affee65d999ea83
Grace Hephzibah M <63595093+Grace-Hephzibah@users.noreply.github.com>
2021-09-11 15:35:47 +0530

    Create End_of_file.c

---
+++ Java/End_of_file.c
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution 
+{
+    public static void main(String[] args) 
+    {        
+        Scanner s = new Scanner(System.in);
+        int i = 0;
+        while(s.hasNext())
+        {
+            i++;
+            System.out.println(i + " " + s.nextLine());
+        }
+    }
+}
